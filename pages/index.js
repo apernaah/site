cc8ddd1b745fbb1a734344fc100605e5dbb36a11
Aparna MS <apernaah@gmail.com>
2026-03-08 04:50:48 +0000

    Clean up header and social components in index.js
    Removed multiple header tags and social links from the main page.

--- pages/index.js
+++ pages/index.js
@@ -48,10 +48,10 @@ export default function Home() {
   }, []);
 
   return (
-    {/* --- DUAL IMAGE RESPONSIVE WRAPPER --- */}
     <div 
       className={`relative min-h-screen w-full bg-cover bg-center bg-no-repeat tablet:bg-fixed bg-[url('/light-bg.jpg')] dark:bg-[url('/dark-bg.jpg')] ${data.showCursor && "cursor-none"}`}
     >
+      {/* --- DUAL IMAGE RESPONSIVE WRAPPER --- */}
       {data.showCursor && <Cursor />}
       <Head>
         <title>{data.name}</title>
@@ -75,50 +75,5 @@ export default function Home() {
             </h1>
             <h2
               ref={textTwo}
-              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
-            >
-              {data.headerTaglineTwo}
-            </h2>
-            <h3
-              ref={textThree}
-              className="text-6xl tablet:text-4xl laptop:text-4xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
-            >
-              {data.headerTaglineThree}
-            </h3>
-            <h3
-              ref={textFour}
-              className="text-6xl tablet:text-4xl laptop:text-4xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
-            >
-              {data.headerTaglineFour}
-            </h3>
-            <h3
-              ref={textFive}
-              className="text-6xl tablet:text-4xl laptop:text-4xl laptopl:text-6xl p-1 tablet:p-2 text-bold w-full laptop:w-4/5"
-            >
-              {data.headerTaglineFive}
-            </h3>
-          </div>
-
-          <Socials className="mt-2 laptop:mt-5" />
-        </div>
-        
-        <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-        </div>
-        {/* This button should not go into production */}
-        {process.env.NODE_ENV === "development" && (
-          <div className="fixed bottom-5 right-5">
-            <Link href="/edit">
-              <Button type="primary">Edit Data</Button>
-            </Link>
-          </div>
-        )}
-        <div className="mt-10 laptop:mt-40 p-2 laptop:p-0" ref={aboutRef}>
-          <p className="tablet:m-10 mt-2 text-xl laptop:text-3xl w-full laptop:w-3/5">
-            {data.aboutpara}
-          </p>
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
-}
+              className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl p-1 p-1 tablet:p-2 text   
+   
